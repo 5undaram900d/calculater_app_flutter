2e12ca8a7a1cff3406557812b45a262814e8e750
5undaram900d <sundaram1105001@gmail.com>
2023-04-27 19:02:31 +0530

    my first commit

---
+++ lib/a02_colors.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+const operatorColor = Color(0xFF272727);
+const buttonColor = Color(0xFF191919);
+const orangeColor = Color(0xFFD9802E);
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:a05_calculater_app/a01_home_main.dart';
+import 'package:flutter/material.dart';
+
+void main() async{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const A01_HomeMain();
+  }
+
+}
+
+
+
